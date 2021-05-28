168ea52532078e7b63cc284c3d1a123741833436
Meriç Gündüz <meric99gunduz@gmail.com>
2021-05-28 14:56:03 +0300

    Js-Ödev 1 added

---
+++ patika-frontend-path homeworks/Javascript- Ödev 1/javascriptclock/js/clock.js
@@ -0,0 +1,19 @@
+function printName(){
+    let isim=prompt("Adınız nedir?");
+    let nameDOC=document.querySelector("#myName");
+    nameDOC.innerHTML=isim;
+}
+
+function showTime(){
+    let clockDOC=document.querySelector("#myClock");
+    let time = new Date().toLocaleTimeString("tr-TR").split(/:| /).join(":");
+
+    let weekdays=["Pazar","Pazartesi","Salı","Çarsamba","Perşembe","Cuma"];
+    let day= weekdays[new Date().getDay()];
+    clockDOC.innerHTML=time+" "+day;
+
+    setTimeout(showTime,1000);
+}
+
+printName();
+showTime();
