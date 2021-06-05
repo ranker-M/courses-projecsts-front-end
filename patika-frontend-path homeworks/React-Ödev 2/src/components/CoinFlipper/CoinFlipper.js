61bb640941a00d6af667074e0b564897eec0623b
Meriç Gündüz <meric99gunduz@gmail.com>
2021-06-05 16:55:13 +0300

    React Odev-2 eklendi

---
+++ patika-frontend-path homeworks/React-Ödev 2/src/components/CoinFlipper/CoinFlipper.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Coin from "../Coin/Coin";
+import "./CoinFlipper.css";
+
+class CoinFlipper extends Component {
+  constructor(props) {
+    super(props);
+    // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
+    // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
+    this.state = {
+      side: "tura",
+      flipping: false,
+      tura:0,
+      yazi:0
+    };
+  }
+  handleClick = () => {
+    // buttona tıklandıgında random sonuc uretiyoruz ve side state'ını ve sonuca iliskin count'u artırarak state'ı guncelliyoruz
+    const yaziOrTura=(prevState)=>{
+      let side=Math.round(Math.random())==0?"tura":"yazi";
+      let updateState={side:side};
+      updateState[side]=prevState[side]+1;
+      return updateState;
+    }
+    this.setState(prevState=>yaziOrTura(prevState));
+    // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
+    this.setState({ flipping: true });
+    // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
+    setTimeout(() => this.setState({ flipping: false }), 1000);
+  };
+
+  render() {
+    return (
+      <div className="CoinFlipper">
+        <h1>Yazı mı Tura mı?</h1>
+        <Coin side={this.state.side} flipping={this.state.flipping} />
+        <button onClick={this.handleClick}>At!</button>
+        <p>
+          Toplam
+          <strong> {this.state.yazi+this.state.tura} </strong>
+          atıştan
+          <strong> {this.state.tura} </strong>ü tura
+          <strong> {this.state.yazi} </strong>
+          si yazı geldi.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default CoinFlipper;
