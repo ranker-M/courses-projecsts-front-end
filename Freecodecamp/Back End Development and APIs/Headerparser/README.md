cdcb13f72cd5a709f368a46ab27c369cf69b70a9
Meriç Gündüz <meric99gunduz@gmail.com>
2021-12-06 19:25:33 +0300

    Headerparser project uploaded

---
+++ Freecodecamp/Back End Development and APIs/Headerparser/README.md
@@ -0,0 +1,5 @@
+# [Request Header Parser Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/request-header-parser-microservice)
+
+<h1>Backend and API Header Parser Project Solution</h1>
+
+I solved this project by using replit boilerplate of freecodecamp. You can run project by uploading these files to replit workspace or you can visit [this](https://replit.com/@ranker-M/boilerplate-project-headerparser) adress to run project on replit.
---
+++ Freecodecamp/Back End Development and APIs/Headerparser/server.js
@@ -0,0 +1,44 @@
+// server.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// My codes
+app.get("/api/whoami",(req,res)=>{
+//   {"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5",
+// "software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+res.json({
+  ipaddress:req.header("X-Forwarded-For"),
+  language: req.header("Accept-language"),
+  software:req.header("user-agent")
+});
+})
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
